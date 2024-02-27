f268400cc62a64bb460fef793d9315221dca9908
Bruce0203 <BruceFreedy@gmail.com>
2024-02-27 21:17:53 +0900

    upd

--- minecraft/src/lib.rs
+++ minecraft/src/lib.rs
@@ -12,12 +12,3 @@ pub mod session_relay;
 pub mod chat;
 
 
-#[test]
-fn test_handshake_server() {
-    println!("Server started!");
-    let mut server = Server::new();
-    let mut selector = Selector::bind("127.0.0.1:25565".parse().unwrap(), 256);
-    selector.start_selection_loop(&mut server);
-}
-
-
--- minecraft/src/test.rs
+++ minecraft/src/test.rs
@@ -1,4 +1,7 @@
 use backtrace::Backtrace;
+use common_server::selector::Selector;
+
+use crate::server::Server;
 
 #[test]
 fn test_backtrace() {
@@ -9,4 +12,13 @@ fn test_backtrace() {
 fn stack_frame_depth() -> usize {
     Backtrace::new_unresolved().frames().len()
 }
+
+#[test]
+fn test_handshake_server() {
+    println!("Server started!");
+    let mut server = Server::new();
+    let mut selector = Selector::bind("127.0.0.1:25565".parse().unwrap(), 256);
+    selector.start_selection_loop(&mut server);
+}
+
 
