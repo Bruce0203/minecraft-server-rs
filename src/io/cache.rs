b7a7452a7c71b544a0f625b96b3ea9557739b722
Bruce0203 <BruceFreedy@gmail.com>
2024-03-06 22:44:53 +0900

    update

--- src/io/cache.rs
+++ src/io/cache.rs
@@ -1,24 +1,26 @@
 use std::{
-    cell::RefCell,
+    cell::{RefCell, UnsafeCell},
     io::{prelude::Write, Result},
     mem::MaybeUninit,
     ops::Deref,
 };
 
+use qcell::{LCell, LCellOwner};
+
 use super::prelude::Encoder;
 
 pub struct Cache<T: Encoder> {
     inner: T,
-    cache: RefCell<MaybeUninit<Vec<u8>>>,
-    is_cached: RefCell<bool>,
+    cache: UnsafeCell<MaybeUninit<Vec<u8>>>,
+    is_cached: UnsafeCell<bool>,
 }
 
 impl<T: Encoder> From<T> for Cache<T> {
     fn from(value: T) -> Self {
         Cache {
             inner: value,
-            cache: RefCell::new(MaybeUninit::uninit()),
-            is_cached: RefCell::new(false),
+            cache: UnsafeCell::new(MaybeUninit::uninit()),
+            is_cached: UnsafeCell::new(false),
         }
     }
 }
@@ -33,15 +35,20 @@ impl<T: Encoder> Deref for Cache<T> {
 
 impl<T: Encoder> Encoder for Cache<T> {
     fn encode_to_write<W: Write>(&self, writer: &mut W) -> Result<()> {
-        let mut is_cached = self.is_cached.borrow_mut();
-        if *is_cached {
-            writer.write_all(unsafe { self.cache.borrow_mut().assume_init_ref() })?;
-        } else {
-            let data = T::encode(self)?.into_inner();
-            writer.write_all(&data)?;
-            self.cache.borrow_mut().write(data);
-            *is_cached = true;
-        }
+        LCellOwner::scope(|mut owner| {
+            let mut is_cached = owner.rw(&self.is_cached);
+            let cache = owner.rw(&self.cache);
+            if *is_cached {
+                writer
+                    .write_all(unsafe { cache.assume_init_ref() })
+                    .unwrap();
+            } else {
+                let data = T::encode(self)?.into_inner();
+                writer.write_all(&data)?;
+                self.cache.borrow_mut().write(data);
+                *is_cached = true;
+            }
+        });
         Ok(())
     }
 }
